d07d8d87eb7ea6516eca3617f7c58c359de98c5c
krazedkrish <krazedkrish@gmail.com>
2015-10-28 22:10:29 +0545

    adds highlight-symbol to highlight symbol

---
+++ highlight-symbol/keybindings.el
@@ -0,0 +1,12 @@
+;; highlight-sysmbol configurations
+(global-set-key [(control f3)] 'highlight-symbol-at-point)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
+(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
+(global-set-key [(control shift f3)] 'unhighlight-regexp)
+(global-set-key [(control shift mouse-1)]
+                (lambda (event)
+                  (interactive "e")
+                  (save-excursion
+                    (goto-char (posn-point (event-start event)))
+                    (highlight-symbol-at-point))))
