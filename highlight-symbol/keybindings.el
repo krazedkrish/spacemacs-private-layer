1a161911a85b43c5a93c4ad299b16c31fd3b7be4
krazedkrish <krazedkrish@gmail.com>
2015-11-13 11:07:09 +0545

    updates keybinding for highlight symblo

--- highlight-symbol/keybindings.el
+++ highlight-symbol/keybindings.el
@@ -1,8 +1,7 @@
 ;; highlight-sysmbol configurations
 (global-set-key [(control f3)] 'highlight-symbol-at-point)
-(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(meta f3)] 'highlight-symbol-next)
 (global-set-key [(shift f3)] 'highlight-symbol-prev)
-(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
 (global-set-key [(control shift f3)] 'unhighlight-regexp)
 (global-set-key [(control shift mouse-1)]
                 (lambda (event)
